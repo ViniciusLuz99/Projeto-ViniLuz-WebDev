8cbee9054444d416f173d7ef1b1465c5ce154aef
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-17 22:09:32 -0300

    Estrutura

---
+++ Assets/js/main.js
@@ -0,0 +1,17 @@
+const mobileMenu = document.getElementById("mobile-menu");
+const openBtn = document.querySelector(".openbtn");
+const navToggler = () => {
+  mobileMenu.classList.toggle("active-menu");
+  openBtn.classList.toggle("active-menu");
+}
+
+
+
+
+
+// function navToggler() {
+//   var mobileMenu = document.getElementById("mobile-menu");
+//   var openBtn = document.querySelector(".openbtn");
+//   mobileMenu.classList.toggle("active");
+//   openBtn.classList.toggle("active");
+// }
