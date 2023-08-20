78fb7c02b9108c2093b70cefa6b5ae543bc41bce
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-19 21:04:19 -0300

    Estrutura, header 50%

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -3,11 +3,7 @@ const openBtn = document.querySelector(".openbtn");
 const navToggler = () => {
   mobileMenu.classList.toggle("active-menu");
   openBtn.classList.toggle("active-menu");
-}
-
-
-
-
+};
 
 // function navToggler() {
 //   var mobileMenu = document.getElementById("mobile-menu");
