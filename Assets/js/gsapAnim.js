9b7e446cd9bed912f94aaec7c88798e3e124f7c2
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-24 16:47:17 -0300

    Melhorada animação da pagina

--- Assets/js/gsapAnim.js
+++ Assets/js/gsapAnim.js
@@ -1,15 +1,52 @@
+//fade up animation
 document.addEventListener("DOMContentLoaded", function () {
-  const animatedElements = document.querySelectorAll('[data-scroll]');
+  const fadeUpElements = document.querySelectorAll('[data-scroll="fade-up"]');
 
-  animatedElements.forEach((element) => {
-    gsap.to(element, {
-      opacity: 1,
-      y: 0,
+  fadeUpElements.forEach((element) => {
+    gsap.from(element, {
+      opacity: 0,
+      y: 50, // Initial position slightly below (off the screen)
+      duration: 1.2,
+      scrollTrigger: {
+        trigger: element,
+        start: "top 80%", // Adjust the value as needed
+      },
+    });
+  });
+});
+//right fade animation
+document.addEventListener("DOMContentLoaded", function () {
+  const animatedElementsFromRight = document.querySelectorAll(
+    '[data-scroll="right"]'
+  );
+
+  animatedElementsFromRight.forEach((element) => {
+    gsap.from(element, {
+      opacity: 0,
+      x: 100, // Initial position off the right side
       duration: 1.5,
       scrollTrigger: {
         trigger: element,
-        start: 'top 100%', // Ajuste o valor conforme necessário
-      }
+        start: "top 100%", // Adjust the value as needed
+      },
+    });
+  });
+});
+//left fade animation
+document.addEventListener("DOMContentLoaded", function () {
+  const animatedElementsFromLeft = document.querySelectorAll(
+    '[data-scroll="left"]'
+  );
+
+  animatedElementsFromLeft.forEach((element) => {
+    gsap.from(element, {
+      opacity: 0,
+      x: -100, // Initial position off the left side
+      duration: 1.5,
+      scrollTrigger: {
+        trigger: element,
+        start: "top 100%", // Adjust the value as needed
+      },
     });
   });
 });
